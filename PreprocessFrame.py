7f696649e8edb0586bcd2560efd46b4ef9ebfb11
zackere <40472142+zackere@users.noreply.github.com>
2018-12-02 17:34:55 +0100

    Delete PreprocessFrame.py

--- PreprocessFrame.py
+++
@@ -1,8 +0,0 @@
-from skimage import transform
-import warnings
-warnings.filterwarnings('ignore')
-def PreprocessFrame(frame):
-	return_frame=rgb2gray(frame) #greyscale image
-	return_frame=transform.resize(return_frame,[240,240]); #crop image
-	return_frame=return_frame/255.0 #normalize colors
-	return return_frame
